c829f91add0c9ae89d4cdd90ae9fd9b20c531c4f
Cynthia Pan <cynthiapan11@gmail.com>
2022-02-04 21:03:43 -0800

    worked on coordinate shifting

--- rmsd.py
+++ rmsd.py
@@ -1,6 +1,8 @@
-from Bio.PDB import *
-import pandas as pd
 import sys
+from Bio.PDB import PDBParser
+import pandas as pd
+import numpy as np
+from numpy import linalg as LA
 
 # command to run the program: python3 rmsd.py structure1_file_path structure2_file_path
 
@@ -9,12 +11,12 @@ import sys
 
 # error handling, check if correct number of arguments passed in
 if len(sys.argv) != 3:
-    raise TypeError("Wrong number of arguments, 3 expected, {} given".format(len(sys.argv)))
+    raise TypeError(
+        "Wrong number of arguments, 3 expected, {} given".format(len(sys.argv)))
 
 # data preprocessing
-def getCoordinates(structure):
-    """
-    Isolate lines that correspond to an alpha carbon (CA)
+def get_coordinates(structure):
+    """Isolate lines that correspond to an alpha carbon (CA)
     and extract the coordinates
 
     Args:
@@ -30,12 +32,26 @@ def getCoordinates(structure):
             data.append(atom.get_coord())
     return data
 
+
 parser = PDBParser()
 
 # structure 1
-structure = parser.get_structure("STRUCT-1", sys.argv[1])
-coord1 = pd.DataFrame(getCoordinates(structure), columns = ["x", "y", "z"])
+structure1 = parser.get_structure("STRUCT-1", sys.argv[1])
+coord1 = pd.DataFrame(get_coordinates(structure1), columns=["c1", "c2", "c3"])
 
 # structure 2
-structure = parser.get_structure("STRUCT-2", sys.argv[2])
-coord2 = pd.DataFrame(getCoordinates(structure), columns = ["x", "y", "z"])
+structure2 = parser.get_structure("STRUCT-2", sys.argv[2])
+coord2 = pd.DataFrame(get_coordinates(structure2), columns=["c1", "c2", "c3"])
+
+# get barycenters
+coord1_means = coord1.mean(axis=1)
+coord2_means = coord2.mean(axis=1)
+
+# shift the coordinates to the barycenters
+def shift_coordinates(coord, means):
+    return coord - means
+
+coord1[["c1", "c2", "c3"]] = coord1[["c1", "c2", "c3"]].apply(
+    shift_coordinates, means=coord1_means)
+coord2[["c1", "c2", "c3"]] = coord2[["c1", "c2", "c3"]].apply(
+    shift_coordinates, means=coord2_means)
