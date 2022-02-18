79dd71dcc771faf8850b03b698347c15d8b0d60b
pthtran22 <92424532+pthtran22@users.noreply.github.com>
2022-02-18 12:49:52 -0800

    changed precision func to more efficient

--- precision.py
+++ precision.py
@@ -6,101 +6,17 @@ from rmsd import calc_RMSD, create_structure, get_coordinates
 
 def precision(coord1, coord2, coord3, coord4, coord5):
 	print("precision function")
-	# rmsd = calc_RMSD(coord1, coord2)
-	# print("rmsd: ", rmsd)	
-	wholeArray = []
-	"""for row in range(len(sys.argv)-1):
-		subArray = []
-		struct1 = sys.argv[row+1]
-		print("row: " + str(row))
-		for col in range(len(sys.argv)-1):
-	 		struct2 = sys.argv[col+1]
-	 		rmsd = calc_RMSD(struct1, struct2)
-	 		print("rmsd:", rmsd)
-	 		subArray.append(rmsd)
-	 		print("col: " + str(col))
-		print("subArray: ")
-		print(subArray)
-		wholeArray.append(subArray)
-	"""
-	
-	# print(calc_RMSD(coord1, coord2))
-#	for row in range(len(sys.argv)-1):
-#		for col in range(len(sys.argv)-1):
-#			print(calc_RMSD("coord"+str(row), "coord"+str(col)))
+	allCoords = [coord1, coord2, coord3, coord4, coord5]
 
-	subArray1 = []
-	pre1_1 = calc_RMSD(coord1, coord1)
-	subArray1.append(pre1_1)
-	pre1_2 = calc_RMSD(coord1, coord2)
-	subArray1.append(pre1_2)
-	pre1_3 = calc_RMSD(coord1, coord3)
-	subArray1.append(pre1_3)
-	pre1_4 = calc_RMSD(coord1, coord4)
-	subArray1.append(pre1_4)
-	pre1_5 = calc_RMSD(coord1, coord5)
-	subArray1.append(pre1_5)
-	print(subArray1)	
-	wholeArray.append(subArray1)
+	predictArray = []
+	for coordRow in allCoords:
+		smallArray = []
+		for coordCol in allCoords:
+			rmsd = calc_RMSD(coordRow, coordCol)
+			smallArray.append(rmsd)
+		predictArray.append(smallArray)
 
-
-	subArray2 = []
-	pre2_1 = calc_RMSD(coord2, coord1)
-	subArray2.append(pre2_1)
-	pre2_2 = calc_RMSD(coord2, coord2)
-	subArray2.append(pre2_2)
-	pre2_3 = calc_RMSD(coord2, coord3)
-	subArray2.append(pre2_3)
-	pre2_4 = calc_RMSD(coord2, coord4)
-	subArray2.append(pre2_4)
-	pre2_5 = calc_RMSD(coord2, coord5)
-	subArray2.append(pre2_5)      
-	# print(subArray2)
-	wholeArray.append(subArray2)
-
-	subArray3 = []
-	pre3_1 = calc_RMSD(coord3, coord1)
-	subArray3.append(pre3_1)
-	pre3_2 = calc_RMSD(coord3, coord2)
-	subArray3.append(pre3_2)
-	pre3_3 = calc_RMSD(coord3, coord3)
-	subArray3.append(pre3_3)
-	pre3_4 = calc_RMSD(coord3, coord4)
-	subArray3.append(pre3_4)
-	pre3_5 = calc_RMSD(coord3, coord5)
-	subArray3.append(pre3_5)
-	# print(subArray3)
-	wholeArray.append(subArray3)
-
-	subArray4 = []
-	pre4_1 = calc_RMSD(coord4, coord1)
-	subArray4.append(pre4_1)
-	pre4_2 = calc_RMSD(coord4, coord2)
-	subArray4.append(pre4_2)
-	pre4_3 = calc_RMSD(coord4, coord3)
-	subArray4.append(pre4_3)
-	pre4_4 = calc_RMSD(coord4, coord4)
-	subArray4.append(pre4_4)
-	pre4_5 = calc_RMSD(coord4, coord5)
-	subArray4.append(pre4_5)
-	# print(subArray4)
-	wholeArray.append(subArray4)
-
-	subArray5 = []
-	pre5_1 = calc_RMSD(coord5, coord1)
-	subArray5.append(pre5_1)
-	pre5_2 = calc_RMSD(coord5, coord2)
-	subArray5.append(pre5_2)
-	pre5_3 = calc_RMSD(coord5, coord3)
-	subArray5.append(pre5_3)
-	pre5_4 = calc_RMSD(coord5, coord4)
-	subArray5.append(pre5_4)
-	pre5_5 = calc_RMSD(coord5, coord5)
-	subArray5.append(pre5_5)
-	# print(subArray5)
-	wholeArray.append(subArray5)
-
-	print(wholeArray)			
+	print(predictArray)			
 
 def main():
 	# error handling, check if correct number of arguments passed in
