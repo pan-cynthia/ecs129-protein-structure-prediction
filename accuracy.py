d398e329a618090b91e9abb6b9db2439f44fb0ee
pan-cynthia <cypan@ucdavis.edu>
2022-02-17 16:47:58 -0800

    clean up

--- accuracy.py
+++ accuracy.py
@@ -1,34 +1,39 @@
 import sys
-from Bio.PDB import PDBParser
 import pandas as pd
 import numpy as np
-import rmsd_func
+from rmsd import calc_RMSD, create_structure, get_coordinates
 
-
-if len(sys.argv) != 3:
-	raise TypeError(
-        "Wrong number of arguments, 3 expected, {} given".format(len(sys.argv)))
-
-#struct1 = sys.argv[2]
-#struct2 = sys.argv[3]
-#struct3 = sys.argv[4]
-#struct4 = sys.argv[5]
-#struct5 = sys.argv[6]
-
-#goldSequence = sys.argv[1]
-
-def accuracy():
+def accuracy(coord_gold, coord1, coord2, coord3, coord4, coord5):
 	print("accuracy function")
-	accuracyArray = []
-	goldSequence = sys.argv[1]
-	for row in range(len(sys.argv)-1):
-		struct = sys.argv[row+1]
-		rmsd = rmsd_func.main(struct, goldSequence)
-		accuracyArray.append(subArray)
-	print(accuracyArray)
-
+	# accuracy_array = []
+	# for row in range(len(sys.argv)-1):
+	# 	struct = sys.argv[row+1]
+	# 	rmsd = rmsd_func.main(struct, goldSequence)
+	# 	accuracyArray.append(subArray)
+	# 	print(accuracyArray)
 
 def main():
-	accuracy()
+	# error handling, check if correct number of arguments passed in
+	if len(sys.argv) != 7:
+		raise TypeError("Wrong number of arguments, 7 expected, {} given".format(len(sys.argv)))
 
-main()	
+	# create structure objects
+	gold_standard = create_structure("GOLD", sys.argv[1])
+	model1 = create_structure("MODEL-1", sys.argv[2])
+	model2 = create_structure("MODEL-2", sys.argv[3])
+	model3 = create_structure("MODEL-3", sys.argv[4])
+	model4 = create_structure("MODEL-4", sys.argv[5])
+	model5 = create_structure("MODEL-5", sys.argv[6])
+
+	# get coordinates
+	coord_gold = pd.DataFrame(get_coordinates(gold_standard), columns=["c1", "c2", "c3"])
+	coord1 = pd.DataFrame(get_coordinates(model1), columns=["c1", "c2", "c3"])
+	coord2 = pd.DataFrame(get_coordinates(model2), columns=["c1", "c2", "c3"])
+	coord3 = pd.DataFrame(get_coordinates(model3), columns=["c1", "c2", "c3"])
+	coord4 = pd.DataFrame(get_coordinates(model4), columns=["c1", "c2", "c3"])
+	coord5 = pd.DataFrame(get_coordinates(model5), columns=["c1", "c2", "c3"])
+
+	accuracy(coord_gold, coord1, coord2, coord3, coord4, coord5)
+
+if __name__ == "__main__":
+	main()
--- precision.py
+++ precision.py
@@ -2,47 +2,49 @@ import sys
 from Bio.PDB import PDBParser
 import pandas as pd
 import numpy as np
-import rmsd_func
+from rmsd import calc_RMSD, create_structure, get_coordinates
 
-
-if len(sys.argv) != 3:
-	raise TypeError(
-        "Wrong number of arguments, 3 expected, {} given".format(len(sys.argv)))
-
-#struct1 = sys.argv[1]
-#struct2 = sys.argv[2]
-#struct3 = sys.argv[3]
-#struct4 = sys.argv[4]
-#struct5 = sys.argv[5]
-
-def precision():
+def precision(coord1, coord2, coord3, coord4, coord5):
 	print("precision function")
-	#rmsd = rmsd_func.main(struct1, struct2)
-	#print("rmsd: ")	
-	wholeArray = []
-	for row in range(len(sys.argv)-1):
-		subArray = []
-		struct1 = sys.argv[row+1]
-		print("row: " + str(row))
-		for col in range(len(sys.argv)-1):
-			struct2 = sys.argv[col+1]
-			rmsd = rmsd_func.main(struct1, struct2)
-			print("rmsd:")
-			print(rmsd)
-			subArray.append(rmsd)
-			print("col: " + str(col))
-		print("subArray: ")
-		print(subArray)
-		wholeArray.append(subArray)
-	print("wholeArray")
-	print(wholeArray)
-
-#def accuracy():
-	#print("accuracy function")
-				
+	rmsd = calc_RMSD(coord1, coord2)
+	print("rmsd: ", rmsd)	
+	# wholeArray = []
+	# for row in range(len(sys.argv)-1):
+	# 	subArray = []
+	# 	struct1 = sys.argv[row+1]
+	# 	print("row: " + str(row))
+	# 	for col in range(len(sys.argv)-1):
+	# 		struct2 = sys.argv[col+1]
+	# 		rmsd = rmsd_func.main(struct1, struct2)
+	# 		print("rmsd:", rmsd)
+	# 		subArray.append(rmsd)
+	# 		print("col: " + str(col))
+	# 	print("subArray: ")
+	# 	print(subArray)
+	# 	wholeArray.append(subArray)
+	# print("wholeArray")
+	# print(wholeArray)			
 
 def main():
-	precision()
-	#accuracy()
+	# error handling, check if correct number of arguments passed in
+	if len(sys.argv) != 6:
+		raise TypeError("Wrong number of arguments, 6 expected, {} given".format(len(sys.argv)))
 
-main()	
+	# create structure objects
+	model1 = create_structure("MODEL-1", sys.argv[1])
+	model2 = create_structure("MODEL-2", sys.argv[2])
+	model3 = create_structure("MODEL-3", sys.argv[3])
+	model4 = create_structure("MODEL-4", sys.argv[4])
+	model5 = create_structure("MODEL-5", sys.argv[5])
+
+	# get coordinates
+	coord1 = pd.DataFrame(get_coordinates(model1), columns=["c1", "c2", "c3"])
+	coord2 = pd.DataFrame(get_coordinates(model2), columns=["c1", "c2", "c3"])
+	coord3 = pd.DataFrame(get_coordinates(model3), columns=["c1", "c2", "c3"])
+	coord4 = pd.DataFrame(get_coordinates(model4), columns=["c1", "c2", "c3"])
+	coord5 = pd.DataFrame(get_coordinates(model5), columns=["c1", "c2", "c3"])
+	
+	precision(coord1, coord2, coord3, coord4, coord5)
+
+if __name__ == "__main__":
+	main()
