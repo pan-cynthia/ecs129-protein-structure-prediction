5fee6fa0ba900b1f971e8a42b126a577cd16b929
pan-cynthia <cypan@ucdavis.edu>
2022-02-19 14:04:08 -0800

    update method of obtaining structures and coordinates

--- accuracy.py
+++ accuracy.py
@@ -1,35 +1,33 @@
 import sys
 import pandas as pd
-import numpy as np
 from rmsd import calc_RMSD, create_structure, get_coordinates
 
 def accuracy(coords):
-	print("accuracy function")
-	coords_gold = coords[0]
-	accuracy_array = []
-	for coord in coords[1:]:
-		e = calc_RMSD(coord, coords_gold)
-		accuracy_array.append(e)
-	print(accuracy_array)
+    coords_gold = coords[0]
+    accuracy = []
+    for coord in coords[1:]:
+        e = calc_RMSD(coord, coords_gold)
+        accuracy.append(e)
+    print(pd.DataFrame([accuracy], index = ["Accuracy"], columns = ["Model 1", "Model 2", "Model 3", "Model 4", "Model 5"]))
 
 def main():
-	# error handling, check if correct number of arguments passed in
-	if len(sys.argv) < 3:
-		raise TypeError("Wrong number of arguments, at least 3 expected, {} given".format(len(sys.argv)))
+    # error handling, check if correct number of arguments passed in
+    if len(sys.argv) != 7:
+        raise TypeError("Wrong number of arguments, 7 expected, {} given".format(len(sys.argv)))
 
-	# create structure objects
-	structures = []
-	for i in range(1,len(sys.argv)):
-		struc = create_structure("MODEL" + str(i),sys.argv[i])
-		structures.append(struc)
+    # create structure objects
+    structures = []
+    for i in range(1,len(sys.argv)):
+        struc = create_structure("MODEL" + str(i),sys.argv[i])
+        structures.append(struc)
 
-	# get coordinates
-	coords = []
-	for struc in structures:
-		coord = pd.DataFrame(get_coordinates(struc), columns=["c1", "c2", "c3"])
-		coords.append(coord)
+    # get coordinates
+    coords = []
+    for struc in structures:
+        coord = pd.DataFrame(get_coordinates(struc), columns = ["c1", "c2", "c3"])
+        coords.append(coord)
 
-	accuracy(coords)
+    accuracy(coords)
 
 if __name__ == "__main__":
-	main()
+    main()
